7f9eb11641c4c9669d7dfe9141b788060995016c
jsk33 <jason33kim@gmail.com>
2019-11-13 21:55:50 -0500

    created Streaks iOS project

---
+++ Streaks/Streaks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Streaks
+//
+//  Created by Jason Kim on 2019-11-13.
+//  Copyright © 2019 Jason Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
