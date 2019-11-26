f251312bd2b6f6cdf875a79af2659d4ebcb7bed7
jsk33 <jason33kim@gmail.com>
2019-11-25 21:11:12 -0500

    swift lesson 1

---
+++ Streaks/SwiftLesson1.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+// this is a comment
+var str = "Hello, playground"
+print(str)
+
+str = "Hello, Jason"
+print(str)
+
+let con = "let is a constant"
+print(con)
+// this won't work because con is a let, which is a constant: con = "hello"
+
+var b = false
+print(b)
+
+var i = 32
+i = 0
+i = -10
+
+var f = 0.3493
+
+
