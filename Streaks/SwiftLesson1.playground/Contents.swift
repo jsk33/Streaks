e91f39b7444b32a14bf8071bddc82cc66b18ee0e
jsk33 <jason33kim@gmail.com>
2019-11-25 21:14:47 -0500

    how to explicitly declare var type

--- Streaks/SwiftLesson1.playground/Contents.swift
+++ Streaks/SwiftLesson1.playground/Contents.swift
@@ -17,6 +17,11 @@ print(b)
 var i = 32
 i = 0
 i = -10
+// can't assign decimal values to i because i is of integer type
+
+// this is how you explicitly declare variable type
+var i2:Double = 32
+print(i2)
 
 var f = 0.3493
 
