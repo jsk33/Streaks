b9ff9ffe9e93032ae9b88e70e6854de9184a9747
jsk33 <jason33kim@gmail.com>
2019-11-26 22:04:11 -0500

    added some functions

--- Streaks/SwiftLesson2.playground/Contents.swift
+++ Streaks/SwiftLesson2.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// basic function
+func sayHello() {
+    print("Hello World")
+}
+
+sayHello()
+
+// function with paramters
+func sayHelloTo(_ name: String, _ age: Int) {
+    print("Hello \(name), you're \(age) years old")
+}
+
+sayHelloTo("Jason", 24)
+
+// function with return value
+func addFourTo(x: Int) -> Int {
+    let sum = x + 4
+    return sum
+}
+
+print(addFourTo(x: 15))
