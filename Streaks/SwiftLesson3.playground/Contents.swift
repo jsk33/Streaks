d8284d34b55c0cb3754842fd699c212d80ae54e1
jsk33 <jason33kim@gmail.com>
2019-11-27 22:37:03 -0500

    played around with a swift class

--- Streaks/SwiftLesson3.playground/Contents.swift
+++ Streaks/SwiftLesson3.playground/Contents.swift
@@ -2,15 +2,24 @@ import UIKit
 
 class Spaceship {
     var fuelLevel = 100
+    var name = ""
     
     
-    func cruise(<#parameters#>) -> <#return type#> {
+    func cruise() {
         // code to initiate cruising
-        
+        print("Cruising is initiated for \(name)")
     }
     
-    func thrust(<#parameters#>) -> <#return type#> {
+    func thrust() {
         // code to initiate rocket thrusters
-        
+        print("Rocket thrusters initiated for \(name)")
     }
 }
+
+var myShip = Spaceship()
+myShip.name = "Enterprise"
+myShip.cruise()
+myShip.thrust()
+print(myShip.fuelLevel)
+myShip.name = "Millenium Falcon"
+print(myShip.name)
