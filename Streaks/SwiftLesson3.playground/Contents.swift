b6ecd7e8976d4bcb0db2f82a69367463b3f47882
jsk33 <jason33kim@gmail.com>
2019-11-27 22:27:40 -0500

    swift class

---
+++ Streaks/SwiftLesson3.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class Spaceship {
+    var fuelLevel = 100
+    
+    
+    func cruise(<#parameters#>) -> <#return type#> {
+        // code to initiate cruising
+        
+    }
+    
+    func thrust(<#parameters#>) -> <#return type#> {
+        // code to initiate rocket thrusters
+        
+    }
+}
