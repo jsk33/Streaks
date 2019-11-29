b47d2ac51fe4dbb6fd68249bd7ceceec10a77e31
jsk33 <jason33kim@gmail.com>
2019-11-28 22:20:38 -0500

    Use IBOutlet properties to access elements in storyboard, IBAction functions hooked up to events in storyboard

--- Streaks/Streaks/ViewController.swift
+++ Streaks/Streaks/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // hooked up the image to this property in the view controller
+    // we now have a reference to the image object
+    // we can call methods on it, configure its properties, etc. inside the view controller here
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    // this function is connected to an event triggered by an object in the storyboard
+    // the sender parameter is a reference to the button that triggered this event
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        print("Deal Tapped")
+        
+    }
+    
 }
 
