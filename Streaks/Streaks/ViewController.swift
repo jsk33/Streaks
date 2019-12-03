15bb3af6c9f58b47ffd78c3ea514254ebd9ec32b
jsk33 <jason33kim@gmail.com>
2019-12-03 00:37:28 -0500

    display saved count when app loads

--- Streaks/Streaks/ViewController.swift
+++ Streaks/Streaks/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         buildAppCount = persist.integer(forKey: "persistKey")
+        buildAppLabel.text = String(buildAppCount)
     }
     
     
