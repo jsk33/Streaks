c46313542bcc244dd2c703b676581c52e5fe68b8
jsk33 <jason33kim@gmail.com>
2019-12-08 14:05:04 -0500

    long press button to reset count works

--- Streaks/Streaks/ViewController.swift
+++ Streaks/Streaks/ViewController.swift
@@ -10,51 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var buildAppLabel: UILabel!
+    
+    @IBOutlet weak var buildAppButton: UIButton!
+    @IBOutlet weak var buildAppNumber: UILabel!
     var buildAppCount:Double = 0
     let buildAppPersistantData = UserDefaults.standard
-    
     @IBOutlet weak var buildAppStepper: UIStepper!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         buildAppCount = buildAppPersistantData.double(forKey: "persistKey")
-        buildAppLabel.text = String(buildAppCount)
+        buildAppNumber.text = String(buildAppCount)
         buildAppStepper.value = buildAppCount
-        
-        let longPressBuildApp = UILongPressGestureRecognizer(target: buildAppLabel, action:#selector(buildAppLongPress(press:)) )
-        longPressBuildApp.minimumPressDuration = 2.0
-        buildAppLabel.addGestureRecognizer(longPressBuildApp)
     }
     
     
     // this function is connected to an event triggered by an object in the storyboard
     // the sender parameter is a reference to the button that triggered this event
-    
-//    @IBAction func buildAppButton(_ sender: UILongPressGestureRecognizer) {
-//        if sender.state == .began {
-//            buildAppCount = 0
-//            buildAppLabel.text = String(buildAppCount)
-//            buildAppStepper.value = buildAppCount
-//            buildAppPersistantData.set(buildAppCount, forKey: "persistKey")
-//        }
-//    }
-    
-    
-    @objc func buildAppLongPress(press: UILongPressGestureRecognizer) {
-        if press.state == .began {
-            buildAppCount = 0
-            buildAppLabel.text = String(buildAppCount)
-            buildAppStepper.value = buildAppCount
-            buildAppPersistantData.set(buildAppCount, forKey: "persistKey")
-        }
+    @IBAction func Stepper(_ sender: UIStepper) {
+        buildAppNumber.text = String(sender.value)
+        buildAppPersistantData.set(sender.value, forKey: "persistKey")
     }
     
     
-    @IBAction func Stepper(_ sender: UIStepper) {
-        buildAppLabel.text = String(sender.value)
-        buildAppPersistantData.set(sender.value, forKey: "persistKey")
+    @IBAction func buildAppLongPress(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            buildAppCount = 0
+            buildAppNumber.text = String(buildAppCount)
+            buildAppStepper.value = buildAppCount
+            buildAppPersistantData.set(buildAppCount, forKey: "persistKey")
+        }
     }
 }
 
